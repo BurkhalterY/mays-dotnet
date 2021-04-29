57ea8253833b9699b28fcf13c1e4e912e4bdb9ca
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-04-29 09:49:17 +0200

    Create tests project

---
+++ Epsic.Info3e.Mays.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace Epsic.Info3e.Mays.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
