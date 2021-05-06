a894456d464c61146e90ef878a393b1af4bab11e
BurkhalterY <33062656+BurkhalterY@users.noreply.github.com>
2021-05-06 08:58:03 +0200

    Add comment model

---
+++ Epsic.Info3e.Mays/Models/Comment.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
+
+namespace Epsic.Info3e.Mays.Models
+{
+    public class Comment
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [JsonIgnore]
+        public string Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; }
+        [JsonIgnore]
+        public IdentityUser Author { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public bool IsSpoiler { get; set; }
+    }
+
+    public class CommentDto
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public UserDto Author { get; set; }
+        public string Content { get; set; }
+        public bool IsSpoiler { get; set; }
+    }
+}
