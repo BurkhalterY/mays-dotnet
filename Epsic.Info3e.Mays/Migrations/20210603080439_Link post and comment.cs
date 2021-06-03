68b99ee80f69280245f4a2351ebb768104a2acb0
medsal15 <medsal15@users.noreply.github.com>
2021-06-03 10:05:13 +0200

    Add migration for post and comment linking

---
+++ Epsic.Info3e.Mays/Migrations/20210603080439_Link post and comment.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Epsic.Info3e.Mays.Migrations
+{
+    public partial class Linkpostandcomment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PostId",
+                table: "Comments",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Comments_Posts_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments");
+
+            migrationBuilder.DropColumn(
+                name: "PostId",
+                table: "Comments");
+        }
+    }
+}
